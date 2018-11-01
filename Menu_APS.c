a466a916954294ffc69e5032e599ff912eff937b
GianL <gianluccaseverino@gmail.com>
2018-10-31 23:45:55 -0300

    master

--- Menu_APS.c
+++ Menu_APS.c
@@ -9,58 +9,59 @@
 #define v3 1000
 
 /*Variaveis auxiliadoras que irao usar em todos metodos de ordenaçao ao longo do programa ... */
-int i,j,aux; 
+int i,j,aux;
 /* Aqui fica todas as funções a serem usadas do programa desde a geracao de vetores
    ate suas ordenaçoes ou menus */
 int menu_VETOR();  //Opcao onde usuario ira escolher tamanho do Vetor[M] ......
 int menu_ORDENACAO(); // Opcao onde usuario qual metodo de ordenacao irá usar .....
 void BubbleSort(int vetor[],int tam); // Metodo de ordenação BubbleSort
 int main(){
-	
+
 	char resp = 'S';
 	do{
-		/*Aqui ele ira pegar o valor escolhido na Opcao menu_VETOR e ira gera o vetor 
+		/*Aqui ele ira pegar o valor escolhido na Opcao menu_VETOR e ira gera o vetor
 		com tamanho escolhido e mostrando para o usuario */
 		system("cls");
 		int tam = menu_VETOR();
 		int vetor_principial[tam];
-	
+
 		for(i=0;i<tam;i++){
 			vetor_principial[i] = rand() %1000;
 		}
-		
+
 		for(i=0;i<tam;i++){
 			printf("\nV[%d] = %d ",i+1,vetor_principial[i]);
 		}
 		printf("\n\nPressione qualquer tecla para continuar ......");
 		getch();
-		
+
 		do{
-	
+
 			int op_MAIN = menu_ORDENACAO();
-		
+
 			switch(op_MAIN){
-				
+
 				case 1:
 					BubbleSort(vetor_principial,tam);
 				break;
-				
+
 				case 2:
 				break;
-				
+
 				case 3:
 				break;
-				
+
 				default:
+				    printf("\n\nNao foi digitado uma opcao de ordenacao valida!!!\n\n");
 				break;
 			}
-			
-			/*Aqui fica a validacao do usuario do que ele deseja fazer assim que escolher seu vetor e fazer 
+
+			/*Aqui fica a validacao do usuario do que ele deseja fazer assim que escolher seu vetor e fazer
 			  a ordenacao dele onde ele pode escolher entre voltar pro Menu de ordenação, caso ele nao queria
 			  vai para segunda pergunta que seria voltar para o Menu principal caso contrario o programa sera
 			  ecenrrado */
-			printf("\nDeseja fazer uma nova Ordenacação com mesmo vetor ? <S/N>\n");
-			resp = toupper(getch());		
+			printf("\nDeseja fazer uma nova Ordenacao com mesmo vetor ? <S/N>\n");
+			resp = toupper(getch());
 				while(resp != 'S' && resp != 'N'){
 					printf("\nOpcao Invalida !! Tente novamente !!!");
 					printf("\nSomente 'S'  ou  'N'");
@@ -68,11 +69,11 @@ int main(){
 					fflush(stdin);
 					resp = toupper(getche());
 				}
-			
+
 		}while(resp == 'S');
-		
-			printf("\nDseja voltar ao Menu dos Vetores ? <S/N>\n");
-			resp = toupper(getch());		
+
+			printf("\nDeseja voltar ao Menu dos Vetores ? <S/N>\n");
+			resp = toupper(getch());
 				while(resp != 'S' && resp != 'N'){
 					printf("\nOpcao Invalida !! Tente novamente !!!");
 					printf("\nSomente 'S'  ou  'N'");
@@ -84,8 +85,8 @@ int main(){
 
 }
 
-int menu_VETOR(){ 
-	
+int menu_VETOR(){
+
 	int op_VETOR;
 	printf("\nDigite a opcao do vetor que deseja :\n");
 	printf("\n10");
@@ -95,38 +96,43 @@ int menu_VETOR(){
 	fflush(stdin);
 	scanf("%d",&op_VETOR);
 
-	if(op_VETOR == 1){
+	if(op_VETOR == 10){
 		return v1;
 	}
-	
-	else if(op_VETOR == 2){
+
+	else if(op_VETOR == 100){
 		return v2;
 	}
-	
-   else{
+
+   else if (op_VETOR == 1000){
 		return v3;
+	} else {
+	    system("cls");
+	    printf("\Nao foi digitado um valor valido!!!\n\n");
+	    menu_VETOR();
 	}
+
 }
 
 int menu_ORDENACAO(){
-	
+
 	system("cls");
 	int op_ORDENACAO;
 	printf("\nDigite a opcao da ordenacao desejada :\n");
 	printf("\n 1 - Buble Sort");
 	printf("\n-> ");
 	scanf("%d",&op_ORDENACAO);
-	return op_ORDENACAO;	
+	return op_ORDENACAO;
 
 }
 
 void BubbleSort(int vetor[],int tam){
-	
+
 	system("cls");
 	printf("\nOk !! Voce escolheu ....... \n");
-	
+
 	printf("\n----------- Metodo BubbleSort -----------\n");
-		
+
 	for( i = 0; i < tam; i++ )
 	{
 	    for( j = i + 1; j < tam; j++ )
@@ -140,13 +146,13 @@ void BubbleSort(int vetor[],int tam){
 	      }
 	    }
 	}
-	   
+
 	  printf("\n\n----- Elementos ordenados -----\n\n");
-	  
+
 	for( i = 0; i < tam; i++ ) {
-	    printf("\nV[%d] - > %d ",i,vetor[i]); // exibe o vetor ordenado
-	  }  
-		
+	    printf("\nV[%d] - > %d ",i+1,vetor[i]); // exibe o vetor ordenado
+	  }
+
 }
 
 
---
+++ Metodos_Ordenacao/Merge_Sort.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+void mergesort(int e, int d, int v[]);
+void intercala(int e, int meio, int d, int v[]);
+
+
+int main() {
+    int vetor10_2[10], posinicial = 0, tamvetor = 10, i;
+
+    printf("\n\n-------------Metodo Merge Sort-------------\n\n");
+
+    //Criação e Mostragem do Vetor
+    printf("\nCriando Vetor...\n");
+    for(i=0;i<10;i++) {
+        vetor10_2[i] = rand() %100;
+    }
+
+    printf("\n\nVetor Criado:\n");
+    printf("\nIndice:\t Valor:\n");
+    for(i=0;i<10;i++) {
+        printf("%d\t %d\n", i+1, vetor10_2[i]);
+    }
+
+
+//Implementação do Método Merge Sort
+
+    printf("\nOrdenando...\n\n");
+    mergesort(posinicial,tamvetor,vetor10_2);
+
+
+    printf("\n\nVetor Ordenado:\n");
+        printf("\nIndice:\t Valor:\n");
+        for(i=0;i<10;i++) {
+            printf("%d\t %d\n", i+1, vetor10_2[i]);
+        }
+
+    return 0;
+    system("PAUSE");
+}
+
+void mergesort(int e, int d, int v[]) {
+    int meio;
+
+    if(e<d-1) {
+        meio = (e+d)/2;
+        mergesort(e,meio,v);
+        mergesort(meio,d,v);
+        intercala(e,meio,d,v);
+    }
+}
+
+
+void intercala (int e, int meio, int d, int v[]) {
+    int i,j,k,*w;
+
+    w = (int*) malloc ((d-e) * sizeof(int));
+
+    i=e;
+    j=meio;
+    k=0;
+
+    while(i<meio && j<d) {
+        if (v[i] < v[j]) {
+            w[k++] = v[i++];
+        }
+        else {
+            w[k++] = v[j++];
+        }
+    }
+
+    while (i<meio) {
+        w[k++] = v[i++];
+    }
+    while (j<d) {
+        w[k++] = v[j++];
+    }
+    for (i=e;i<d;++i) {
+        v[i] = w[i-e];
+    }
+
+    free(w);
+}
