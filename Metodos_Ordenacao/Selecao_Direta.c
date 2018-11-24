e475f6885403752e7a2c4374fae186d1c020108c
GianL <gianluccaseverino@gmail.com>
2018-11-24 00:04:46 -0200

    master

--- Metodos_Ordenacao/Selecao_Direta.c
+++ Metodos_Ordenacao/Selecao_Direta.c
@@ -3,7 +3,8 @@
 #include <math.h>
 
 int main() {
-    int vetor10_2[10],menor, posmenor, i, j, contador = 0;
+    int vetor[1000000], menor, posmenor, i, j, contador = 0;
+    int tam_vetor=sizeof(vetor)/4;
 
 
 
@@ -11,47 +12,51 @@ int main() {
 
 //Criação e Mostragem do Vetor
     printf("\nCriando Vetor...\n");
-    for(i=0;i<10;i++) {
-        vetor10_2[i] = rand() %100;
+    for(i=0;i<tam_vetor;i++) {
+        vetor[i] = rand() %100;
     }
 
-    printf("\n\nVetor Criado:\n");
-    printf("\nIndice:\t Valor:\n");
-    for(i=0;i<10;i++) {
-        printf("%d\t %d\n", i+1, vetor10_2[i]);
+    printf("\n\nVetor Criado!!!\n");
+    if (tam_vetor < 10000) {
+        printf("\nIndice:\t Valor:\n");
+        for(i=0;i<tam_vetor;i++) {
+            printf("%d\t %d\n", i+1, vetor[i]);
+        }
     }
 
 
 //Implementação do Método Seleção Direta
 
     printf("\nOrdenando...\n\n");
-    for(i=0;i<10;i++){
-        menor=vetor10_2[i];
+    for(i=0;i<tam_vetor;i++){
+        menor=vetor[i];
         posmenor=i;
         //printf("\nMenor = %d",menor);
 
-        for(j=i+1;j<10;j++) {
+        for(j=i+1;j<tam_vetor;j++) {
         // Verificação do valor ser menor que o primeiro capturado, a partir do elemento seguinte em diante
        // printf("\nMenor 2no J = %d ",menor);
        // printf("\nValor vetor principal = %d",vetor10_2[i]);
-            if(vetor10_2[j]<menor) {
-                menor=vetor10_2[j];
+            if(vetor[j]<menor) {
+                menor=vetor[j];
                 posmenor=j;
             }
             contador++;
-           
+
         }
-        
-            vetor10_2[posmenor]=vetor10_2[i];
-            vetor10_2[i]=menor;
+
+            vetor[posmenor]=vetor[i];
+            vetor[i]=menor;
     }
 
+    printf("\nContagem de Voltas: %d", contador);
+    getch();
     printf("\n\nVetor Ordenado:\n");
     printf("\nIndice:\t Valor:\n");
-    for(i=0;i<10;i++) {
-        printf("%d\t %d\n", i+1, vetor10_2[i]);
+    for(i=0;i<tam_vetor;i++) {
+        printf("%d\t %d\n", i+1, vetor[i]);
     }
-    printf("\nContagem de Voltas: %d", contador);
+
 
 
 
